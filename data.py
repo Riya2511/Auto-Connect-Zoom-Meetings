33c8c007c3f381c7021e08541e472776d463d334
Riya2511 <84588658+Riya2511@users.noreply.github.com>
2021-09-18 21:32:29 +0530

    Update data.py

--- data.py
+++ data.py
@@ -14,15 +14,18 @@ webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("Enter the path o
 current_time=int(datetime.now().strftime("%H%M"))
 start_time1=int(start_time[0:2]+start_time[3:5])
 end_time1=int(end_time[0:2]+end_time[3:5])
-
-if (current_time>=start_time1) and (current_time<=end_time1):              #Comparing if the meeting has already started or is yet to be started
-    webbrowser.get('chrome').open(url)                                     #Opening chrome and loading the url
+while True:
+    if (current_time>=start_time1) and (current_time<=end_time1):              #Comparing if the meeting has already started or is yet to be started
+        webbrowser.get('chrome').open(url)                                     #Opening chrome and loading the url
 
     #You must select the checkbox stating "Always allow us05web.zoom.us to open links of this type in the associated app"
     
-    print("The meeting has started and connected")
-    time.sleep(5)
-    os.system("taskkill /im chrome.exe /f")
+        print("The meeting has started and connected")
+        time.sleep(5)
+        os.system("taskkill /im chrome.exe /f")
+        break
 
-else:
-    print("The zoom meeting has not yet started")
+    else:
+        print("The zoom meeting has not yet started")
+        time.sleep(60)
+        
